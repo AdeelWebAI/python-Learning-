2b454daec6bfe885768469007258d07d0fe06f42
Adeel <adeelwaraich27@gmail.com>
2025-02-06 00:19:24 +0500

    6.2.2025

--- sets.py
+++ sets.py
@@ -10,15 +10,28 @@
 # print(empty)
 # for value in s:
 #     print(value)
-
-s1 = {1,2,3,4}
-s2 = {4,5,6,7}
 # print(s1, s2)
 # print(s1.union(s2))
 # s3 = s1.update(s2)
 # print(s1)
 # s3 = s1.intersection(s2)
+# ss = {"one","two","three","four"}
+# sss= {"three","four","five"}
+# ss.intersection_update(sss)
+# print(ss)
 
-s3 = s1.intersection_update(s2)
+
+
+# s3 = s1.symmetric_difference(s2)
+
+# print(s3)
+
+# s3 = s2.difference(s1)
+
+# print(s3)
+
+s1 = {2,3,4}
+s2 = {4,5,6}
+s3 = s1.isdisjoint(s2)
 
 print(s3)
