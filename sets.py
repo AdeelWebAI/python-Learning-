893c62884d4171a6a65a2f6b071cdfad291c46f4
Adeel <adeelwaraich27@gmail.com>
2025-02-05 23:14:47 +0500

    sets tutorial first day

---
+++ sets.py
@@ -0,0 +1,12 @@
+s = {1,3,5,6,77,7}
+# print(s)
+
+# python sets don't folow the order of the elements
+
+# info = {"align","order","size","doubles"}
+# print(info)
+# s = set()
+# empty = print(type (s))
+# print(empty)
+# for value in s:
+#     print(value)
